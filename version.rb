dd41eb5edec175626a51d377966248ec3ed7d344
Indicado <indi88@riseup.net>
2023-08-26 16:59:52 +0200

    Update NMS download url

--- version.rb
+++ version.rb
@@ -1,4 +1,5 @@
 NMS_VERSION="1.11.1"
 NMS_JAR="cache/nms_#{NMS_VERSION}.jar"
-NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/#{NMS_VERSION}/minecraft_server.#{NMS_VERSION}.jar"
+# NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/#{NMS_VERSION}/minecraft_server.#{NMS_VERSION}.jar" <- Unavailable
+NMS_URL="https://piston-data.mojang.com/v1/objects/1f97bd101e508d7b52b3d6a7879223b000b5eba0/server.jar"
 NMS_MD5="f626d8a0427306c1140b772eaac12405"
